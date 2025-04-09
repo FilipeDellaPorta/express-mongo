dfdd375c6eb06f55ac6139343f6368709f350a8c
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-04-09 18:30:41 -0300

    feat: :sparkles: add: rota de livros json

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from 'express';
 
 const app = express();
 
+const livros = [
+  {
+    id: 1,
+    titulo: 'O Senhor dos Aneis',
+  },
+  {
+    id: 2,
+    titulo: 'Duna',
+  },
+];
+
 app.get('/', (req, res) => {
   res.status(200).send('Curso de Node.js');
+});
+
+app.get('/livros', (req, res) => {
+  res.status(200).json(livros);
 });
 
 export default app;
