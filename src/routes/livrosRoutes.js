03e5df56c4ef72220c1ef972deb2a5447cdb2af9
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-04-12 17:47:00 -0300

    feat: :sparkles: reativado o resto do CRUD e cancelado do Render e MongoDB por enquanto

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -6,10 +6,9 @@ const routes = express.Router();
 routes.get('/livros', LivroController.listarLivros);
 routes.get('/livros/busca', LivroController.listarLivrosPorEditora);
 routes.get('/livros/:id', LivroController.listarLivroPorId);
-/*
 routes.post('/livros', LivroController.criarLivro);
 routes.put('/livros/:id', LivroController.editarLivroPorId);
 routes.delete('/livros/:id', LivroController.deletarLivroPorId); 
-*/
+
 
 export default routes;
