3de74fcea2db3b1b08522721f9f880335a5fac7f
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-04-12 15:32:57 -0300

    feat: :sparkles: add: CRUD para rotas e controladores

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,8 +2,63 @@ import livro from '../models/Livro.js';
 
 class LivroController {
   static async listarLivros(req, res) {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha ao listar livros` });
+    }
+  }
+
+  static async listarLivroPorId(req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha ao listar livro` });
+    }
+  }
+
+  static async criarLivro(req, res) {
+    try {
+      const novoLivro = await livro.create(req.body);
+      res
+        .status(201)
+        .json({ message: 'Livro cadastrado com sucesso!', livro: novoLivro });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha ao cadastrar livro` });
+    }
+  }
+
+  static async editarLivroPorId(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: 'Livro atualizado!' });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha ao listar livro` });
+    }
+  }
+
+  static async deletarLivroPorId(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+      res.status(200).json({ message: 'Livro excluído com sucesso!' });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha ao deletar livro` });
+    }
   }
 }
 
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,3 +4,9 @@ import LivroController from '../controllers/livroController.js';
 const routes = express.Router();
 
 routes.get('/livros', LivroController.listarLivros);
+routes.get('/livros/:id', LivroController.listarLivroPorId);
+routes.post('/livros', LivroController.criarLivro);
+routes.put('/livros/:id', LivroController.editarLivroPorId);
+routes.delete('/livros/:id', LivroController.deletarLivroPorId);
+
+export default routes;
