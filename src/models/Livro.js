8be88b8dcf3d791a07a7176471323354a0f5c368
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-04-16 16:10:05 -0300

    feat: :sparkles: add: mensagens de erro personalizadas

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,11 +4,14 @@ import { autorSchema } from './Autor.js';
 const livroSchema = new mongoose.Schema(
   {
     id: { type: mongoose.Schema.Types.ObjectId },
-    titulo: { type: mongoose.Schema.Types.String, required: true },
+    titulo: {
+      type: mongoose.Schema.Types.String,
+      required: [true, 'título livro obrigatório'],
+    },
     editora: { type: String },
     preco: { type: mongoose.Schema.Types.Number },
     paginas: { type: Number },
-    autor: autorSchema,
+    autor: { autorSchema, required: [true, 'id autor(a) obrigatório'] },
   },
   { versionKey: false }
 );
