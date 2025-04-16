2b3dcef4188b65cebdf67d422bb9cb41209202e1
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-04-16 16:15:17 -0300

    feat: :sparkles: personalizando ainda mais os erros com o Path

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -3,6 +3,15 @@ import mongoose from 'mongoose';
 function manipuladorDeErros(error, req, res, next) {
   if (error instanceof mongoose.Error.CastError) {
     res.status(400).json({ message: 'um ou mais dados estão incorretos' });
+  } else if (error instanceof mongoose.Error.ValidationError) {
+    const mensagensErro = Object.values(error.errors)
+      .map((error) => error.message)
+      .join('; ');
+    res
+      .status(400)
+      .json({
+        message: `os seguintes erros foram encontrados: ${mensagensErro}`,
+      });
   } else {
     res.status(500).json({ message: 'erro interno de servidor' });
   }
