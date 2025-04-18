ad63cb20aa0f7360b06e889442be13ce26e9e952
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-04-18 12:19:35 -0300

    feat: :sparkles: add: ordenação a paginação

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -6,15 +6,18 @@ import RequisicaoIncorreta from '../errors/RequisicaoIncorreta.js';
 class LivroController {
   static async listarLivros(req, res, next) {
     try {
-      let { limite = 5, pagina = 1 } = req.query;
+      let { limite = 5, pagina = 1, ordenacao = '_id:-1' } = req.query;
+
+      let [ordenarLivrosPor, crescenteOuDecrescente] = ordenacao.split(':');
 
       limite = parseInt(limite);
       pagina = parseInt(pagina);
+      crescenteOuDecrescente = parseInt(crescenteOuDecrescente);
 
       if (limite > 0 && pagina > 0) {
         const listaLivros = await livro
           .find({})
-          .sort({ titulo: 1 })
+          .sort({ [ordenarLivrosPor]: crescenteOuDecrescente })
           .skip((pagina - 1) * limite)
           .limit(limite);
         res.status(200).json(listaLivros);
